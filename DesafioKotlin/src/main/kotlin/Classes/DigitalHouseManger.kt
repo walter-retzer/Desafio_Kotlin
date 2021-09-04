d3ff3911d54d9746914533b747a553d620c8f654
WDRETZER <walter_retzer@yahoo.com.br>
2021-09-04 14:57:14 -0300

    Desafio Kotlin - Parte G

---
+++ DesafioKotlin/src/main/kotlin/Classes/DigitalHouseManger.kt
@@ -0,0 +1,24 @@
+package Classes
+
+// 1. Criar um diagrama de classes que modele a classe DigitalHouseManager.
+// A princípio, DigitalHouseManager tem uma lista de alunos, uma lista de professores,
+// uma lista de cursos e uma lista de matrículas
+
+class DigitalHouseManger(
+    var listaAlunos: MutableList<Alunos>,
+    var listaProfessorTitular: MutableList<ProfessorTitular>,
+    var listaProfessorAdjunto: MutableList<ProfessorAdjunto>,
+    var listaCurso: MutableList<Curso>,
+    var listaMatriculas: MutableList<Matricula>
+) {
+
+    //sobreescrevendo a Função toString() para exibir informações da Composição da DigitalHouseManager:
+    override fun toString(): String {
+        return "Digital House Manager composta por: " +
+                "\nLista de Alunos: ${listaAlunos}" +
+                "\nLista de Cursos: ${listaCurso}" +
+                "\nLista de Professores Titulares: ${listaProfessorTitular}" +
+                "\nLista de Professores Adjuntos: ${listaProfessorAdjunto}" +
+                "\nLista de Alunos Matrículados: ${listaMatriculas}"
+    }
+}
--- DesafioKotlin/src/main/kotlin/Classes/Main.kt
+++ DesafioKotlin/src/main/kotlin/Classes/Main.kt
@@ -7,7 +7,7 @@ fun main(){
     //PARTE A: criando as variáveis alunos:
     var aluno = Alunos("Mickey", "Mouse", 1 )
     var aluno1 = Alunos("Minnie", "Mouse", 2 )
-    var lista = mutableListOf(aluno, aluno1)
+    var listaAlunos = mutableListOf(aluno, aluno1)
 
     //comparando se aluno e aluno1 são iguais:
     println(aluno == aluno1)
@@ -25,6 +25,7 @@ fun main(){
        //PARTE D: criando as variáveis dos Professores Adjunto e Titular:
     var professorTitular = ProfessorTitular("Nicloas", "Tesla", 1500, 9901, "Java")
     var professorTitular1 = ProfessorTitular("Stephen", "Hawking", 1354, 9901, "Kotlin")
+    var listaProfessoresTitular = mutableListOf(professorTitular, professorTitular1)
 
     //comparando se professorTitular e professorTitular1 são iguais:
     println(professorTitular == professorTitular1)
@@ -41,6 +42,7 @@ fun main(){
 
     var professorAdjunto = ProfessorAdjunto("Albert", "Einstein", 957,8802, 25954)
     var professorAdjunto1 = ProfessorAdjunto("Isaac", "Newton",875,8802, 22222)
+    var listaProfessorAdjunto = mutableListOf(professorAdjunto, professorAdjunto1)
 
     //comparando se professorAdjunto e professorAdjunto1 são iguais:
     println(professorAdjunto == professorAdjunto1)
@@ -57,8 +59,9 @@ fun main(){
 
     //PARTE E: criando as variáveis do curso:
 
-    var curso = Curso("FullStack", 20001, professorTitular, professorAdjunto, 40, lista)
-    var curso1 = Curso("Android", 20002, professorTitular1, professorAdjunto1, 20, lista)
+    var curso = Curso("FullStack", 20001, professorTitular, professorAdjunto, 40, listaAlunos)
+    var curso1 = Curso("Android", 20002, professorTitular1, professorAdjunto1, 20, listaAlunos)
+    var listaCursos = mutableListOf(curso, curso1)
 
    //comparando se curso e curso1 são iguais:
     println(curso == curso1)
@@ -76,7 +79,19 @@ fun main(){
 
     //PARTE F: criando as variáveis da matricula:
     var matricula = Matricula(aluno,curso)
+    var matricula1 = Matricula(aluno1,curso1)
+    var listaMatriculas = mutableListOf(matricula, matricula1)
+
     println(matricula.dataMatricula)
     println(matricula)
+
+
+    println("---------------------------------------------------------------------------------------------------------")
+
+    //PARTE G: criando as variáveis da matricula:
+    var managerDH = DigitalHouseManger(listaAlunos, listaProfessoresTitular, listaProfessorAdjunto, listaCursos, listaMatriculas)
+    println(managerDH)
+
+    println("---------------------------------------------------------------------------------------------------------")
 
 }
